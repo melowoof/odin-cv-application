9fbc8ae49bf8f8188e3795cd5804d54ee3fb9d7a
Ian <ian.nptk@gmail.com>
2024-11-27 16:59:38 +0100

    Change folder name

---
+++ src/components/common/Input.jsx
@@ -0,0 +1,27 @@
+/* eslint-disable react/prop-types */
+import "../../styles/Input.css";
+
+function Input({ label, type, id, placeholder, value, onChange }) {
+  // const [input, setInput] = useState("");
+
+  // function handleChange(e) {
+  //   setInput(e.target.value);
+  // }
+
+  const inputId = `${label.replace(/\s+/g, "-").toLowerCase()}-input`;
+
+  return (
+    <div className="input-container">
+      {label !== "" && <label htmlFor={inputId}> {label} </label>}
+      <input
+        id={id}
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+      />
+    </div>
+  );
+}
+
+export default Input;
